6b3c0a8ee305cd577ee1cf8263527130967f3d43
yash-wanth6 <yashwanth.rajkumar6@gmail.com>
2025-05-28 14:05:36 +0530

    14:05

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold text-red-500 mb-6"><span className='text-gray-700'><i class="fa-solid fa-globe"></i></span> 404 Page Not Found</h1>
+            <h1 className="text-4xl font-bold text-gray-600 mb-6"><span className='text-gray-600'><i class="fa-solid fa-globe"></i></span> 404 Page Not Found</h1>
             <button className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
                 Go to Home
             </button>
