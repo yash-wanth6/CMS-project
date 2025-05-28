31649aee4667140dc26183b83d96b40ee25db237
yash-wanth6 <yashwanth.rajkumar6@gmail.com>
2025-05-28 15:53:29 +0530

    "15:53"

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Signup from '../pages/Signup'
+import Signin from '../pages/Signin'
+import Aboutpage from '..pages/Aboutpage'
 
 export default function Header() {
-    const paths = []
-    const value = [<Signup></Signup>]
+    const paths = ['../pages/Signup','../pages/Signin','..pages/Aboutpage']
+    const value = [<Signup></Signup>,<Signin></Signin>,<Aboutpage></Aboutpage>]
   return (
     <>
         <h1>Tracker</h1>
-
     </>
   )
 }
