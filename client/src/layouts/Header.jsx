e9ad08d3990eb105eb7769bb2cc10ed6da9e6bf2
yash-wanth6 <yashwanth.rajkumar6@gmail.com>
2025-05-29 09:52:37 +0530

    9:52

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Signup from '../pages/Signup'
 import Signin from '../pages/Signin'
-import Aboutpage from '..pages/Aboutpage'
+import Aboutpage from '../pages/Aboutpage'
+import {Link , Outlet} from 'react-router-dom'
 
 export default function Header() {
-    const paths = ['../pages/Signup','../pages/Signin','..pages/Aboutpage']
-    const value = [<Signup></Signup>,<Signin></Signin>,<Aboutpage></Aboutpage>]
-  return (
-    <>
-        <h1>Tracker</h1>
-    </>
-  )
+    const paths = ['/signup', '/signin', '/aboutpage']
+    const value = ["Sign-up","Sign-in","About"]
+    return (
+        <>
+            <h1>Tracker</h1>
+            <ul>
+                {
+                    paths.map((value, index) => {
+                        return (<li key={index}><Link to={paths[index]}>{value[index]}</Link></li>)
+                    })
+                }
+            </ul>
+            <main>
+                <Outlet></Outlet>
+            </main>
+        </>
+    )
 }
